ca10538685a1396ec54f2e244c680ed0e5bde370
AdrienP <adrien.pinheiro@sogeti.com>
2024-11-22 11:44:45 +0100

    Adding project structure and starting build exercices

---
+++ Angular_Spring/Exercices/src/main/java/com/deep_session/exercices/DeepSessionExercicesApplication.java
@@ -0,0 +1,13 @@
+package com.deep_session.exercices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepSessionExercicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepSessionExercicesApplication.class, args);
+	}
+
+}
