abe2470aba903468529dc74403ca68f804b6f48a
AdrienP <adrien.pinheiro@sogeti.com>
2024-12-03 14:43:10 +0100

    Add angular in all deepdive

---
+++ C#_Angular_Typescript/02 - Challenge/Resolved/front/src/app/components/movie-crud/movie-crud.component.spec.ts
@@ -0,0 +1,87 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { MovieCrudComponent } from './movie-crud.component';
+import { MovieService } from '../../services/movie.service';
+import { Movie } from '../../models/movie.model';
+import { of } from 'rxjs';
+
+describe('MovieCrudComponent', () => {
+  let component: MovieCrudComponent;
+  let fixture: ComponentFixture<MovieCrudComponent>;
+  let movieService: MovieService;
+  const mockMovies: Movie[] = [
+    { id: 1, title: 'Movie 1', director: 'Director 1', releaseYear: 2021 },
+    { id: 2, title: 'Movie 2', director: 'Director 2', releaseYear: 2022 }
+  ];
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, FormsModule, MovieCrudComponent],
+      providers: [MovieService]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MovieCrudComponent);
+    component = fixture.componentInstance;
+    movieService = TestBed.inject(MovieService);
+
+    spyOn(movieService, 'getMovies').and.returnValue(of(mockMovies));
+    spyOn(movieService, 'createMovie').and.returnValue(of(mockMovies[0]));
+    spyOn(movieService, 'updateMovie').and.returnValue(of(void 0));
+    spyOn(movieService, 'deleteMovie').and.returnValue(of(void 0));
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should mock getMovies service', () => {
+    movieService.getMovies().subscribe((movies) => {
+      expect(movies).toEqual(mockMovies); // Verify that the mock works
+    });
+  });
+
+  it('should fetch movies on init', () => {
+    movieService.getMovies().subscribe((movies) => {
+      expect(movies.length).toBe(2);
+    });
+  });
+
+  it('should add a movie', () => {
+    const newMovie: Movie = { id: 3, title: 'Movie 3', director: 'Director 3', releaseYear: 2023 };
+    movieService.getMovies().subscribe((movies) => {
+      movies.push(newMovie);
+      expect(movies.length).toBe(3);
+      expect(movies).toContain(newMovie);
+    });
+  });
+
+  it('should edit a movie', () => {
+    component.editMovie(mockMovies[0]);
+    expect(component.movie).toEqual(mockMovies[0]);
+    expect(component.isEdit).toBeTrue();
+  });
+
+  it('should update a movie', () => {
+    const updatedMovie: Movie = { id: 1, title: 'Updated Movie 1', director: 'Updated Director 1', releaseYear: 2021 };
+    movieService.getMovies().subscribe((movies) => {
+      movies[0] = updatedMovie;
+      expect(movies[0]).toEqual(updatedMovie);
+    });
+  });
+
+  it('should delete a movie', () => {
+    movieService.getMovies().subscribe((movies) => {
+      movies.splice(0, 1);
+      expect(movies.length).toBe(2);
+    });
+  });
+
+  it('should reset the form', () => {
+    component.resetForm();
+    expect(component.movie).toEqual({ id: 0, title: '', director: '', releaseYear: 0 });
+    expect(component.isEdit).toBeFalse();
+  });
+});
---
+++ C#_Angular_Typescript/02 - Challenge/Resolved/front/src/app/components/movie-crud/movie-crud.component.ts
@@ -0,0 +1,64 @@
+// src/app/components/movie-crud/movie-crud.component.ts
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MovieService } from '../../services/movie.service';
+import { Movie } from '../../models/movie.model';
+
+@Component({
+  selector: 'app-movie-crud',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './movie-crud.component.html',
+  styleUrls: ['./movie-crud.component.scss'],
+  providers: [MovieService]
+})
+export class MovieCrudComponent implements OnInit {
+  movies: Movie[] = [];
+  movie: Movie = { title: '', director: '', releaseYear: 0 };
+  isEdit = false;
+
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  getMovies(): void {
+    this.movieService.getMovies().subscribe((movies) => {
+      this.movies = movies;
+    });
+  }
+
+  addMovie(): void {
+    this.movieService.createMovie(this.movie).subscribe((movie) => {
+        this.movies.push(movie);
+        this.resetForm();
+    });
+}
+
+  editMovie(movie: Movie): void {
+    this.movie = { ...movie };
+    this.isEdit = true;
+  }
+
+  updateMovie(): void {
+    console.log(this.movie);    
+    if(this.movie.id === undefined) return;
+    else this.movieService.updateMovie(this.movie.id, this.movie).subscribe(() => {
+        this.getMovies();
+        this.resetForm();
+    });
+}
+
+  deleteMovie(id: number): void {
+    this.movieService.deleteMovie(id).subscribe(() => {
+      this.movies = this.movies.filter((movie) => movie.id !== id);
+    });
+  }
+
+  resetForm(): void {
+    this.movie = { id: 0, title: '', director: '', releaseYear: 0, version: 0 };
+    this.isEdit = false;
+  }
+}
